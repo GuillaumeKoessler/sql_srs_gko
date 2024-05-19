b6855eaa5b8de1310a4dcbec7bd9cb3e53ca1ff3
GuillaumeKoessler <guillaume.koessler@gmail.com>
2024-05-19 18:52:46 +0200

    apply black to app.py

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ import io
 
 # données sources
 
-csv = '''
+csv = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
+"""
 beverages = pd.read_csv(io.StringIO(csv))
 
-csv2 = '''
+csv2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
-'''
+"""
 food_items = pd.read_csv(io.StringIO(csv2))
 
 # query de la solution + table solution
@@ -34,15 +34,17 @@ st.header("enter your code:")
 # ajout de la slide bar
 with st.sidebar:
     type_exec = st.selectbox(
-       "Veuillez choisir une catégorie d'exercice",
-       ("Joins", "GroupBy", "Windows Functions"),
-       index=None,
-       placeholder="Sélection du type d'exercice...",
+        "Veuillez choisir une catégorie d'exercice",
+        ("Joins", "GroupBy", "Windows Functions"),
+        index=None,
+        placeholder="Sélection du type d'exercice...",
     )
     st.write("Vous avez choisi", type_exec)
 
 # Saisie de la requête
-query_str = st.text_area(label="Entrez ici votre requête",)
+query_str = st.text_area(
+    label="Entrez ici votre requête",
+)
 
 # Calcul de la table en sortie
 if query_str:
@@ -54,18 +56,14 @@ if query_str:
         try:
             sortie_df = sortie_df[solution_df.columns]
             st.dataframe(sortie_df.compare(solution_df))
-        except KeyError as e :
+        except KeyError as e:
             st.write("Noms de colonnes non identiques")
 
         nb_line_diff = sortie_df.shape[0] - solution_df.shape[0]
         if nb_line_diff > 0:
-            st.write(
-                f"Le résultat à {nb_line_diff} lignes en trop"
-            )
+            st.write(f"Le résultat à {nb_line_diff} lignes en trop")
         if nb_line_diff < 0:
-            st.write(
-                f"Le résultat à {abs(nb_line_diff)} lignes en moins"
-            )
+            st.write(f"Le résultat à {abs(nb_line_diff)} lignes en moins")
     except:
         st.write("Erreur de syntaxe SQL")
 
