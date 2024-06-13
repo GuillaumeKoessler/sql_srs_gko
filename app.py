5b5dffba689d8fb753c97841ec267b2eb048bb01
GuillaumeKoessler <guillaume.koessler@gmail.com>
2024-06-13 23:12:00 +0200

    make solution_df work again

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ import streamlit as st
 # connections a la base de données
 con = duckdb.connect(database="data/exercices_sql_tables.duckdb", read_only=False)
 
-# query de la solution + table solution
-# ANSWER_STR = """
-# SELECT * FROM beverages
-# CROSS JOIN food_items
-# """
-# solution_df = duckdb.sql(ANSWER_STR).df()
-
 # Mise en page
 st.header("enter your code:")
 
@@ -34,6 +27,14 @@ with st.sidebar:
     ).df()
     st.dataframe(list_exo_sl_df)
 
+    # recherche du script sql solution
+    EXO_NAME_STR = list_exo_sl_df.loc[0, "exercice_name"]
+    with open(f"answers/{EXO_NAME_STR}.sql", "r") as f:
+        answer = f.read()
+        solution_df = con.execute(
+            f"{answer}"
+        ).df()
+
 # Saisie de la requête
 query_str = st.text_area(
     label="Entrez ici votre requête",
@@ -43,21 +44,19 @@ query_str = st.text_area(
 if query_str:
     try:
         sortie_df = con.execute(query_str).df()
-        # sortie_df = duckdb.sql(query_str).df()
         # Affichage de la sortie
         st.write("Table en sortie :")
         st.dataframe(sortie_df)
-        # try:
-        #     sortie_df = sortie_df[solution_df.columns]
-        #     st.dataframe(sortie_df.compare(solution_df))
-        # except KeyError as e:
-        #     st.write("Noms de colonnes non identiques")
-        #
-        # nb_line_diff = sortie_df.shape[0] - solution_df.shape[0]
-        # if nb_line_diff > 0:
-        #     st.write(f"Le résultat à {nb_line_diff} lignes en trop")
-        # if nb_line_diff < 0:
-        #     st.write(f"Le résultat à {abs(nb_line_diff)} lignes en moins")
+        try:
+            sortie_df = sortie_df[solution_df.columns]
+            st.dataframe(sortie_df.compare(solution_df))
+        except KeyError as e:
+            st.write("Noms de colonnes non identiques")
+            nb_line_diff = sortie_df.shape[0] - solution_df.shape[0]
+        if nb_line_diff > 0:
+            st.write(f"Le résultat à {nb_line_diff} lignes en trop")
+        if nb_line_diff < 0:
+            st.write(f"Le résultat à {abs(nb_line_diff)} lignes en moins")
     except:
         st.write("Erreur de syntaxe SQL")
 
@@ -72,11 +71,8 @@ with tab2:
             f"SELECT * FROM {tbl}"
         ).df()
         st.dataframe(table_print)
-    # st.write("expected:")
-    # st.dataframe(solution_df)
+    st.write("expected:")
+    st.dataframe(solution_df)
 
 with tab3:
-    ANSWER_STR = list_exo_sl_df.loc[0, "exercice_name"]
-    with open(f"answers/{ANSWER_STR}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
